8d98a7ce63c697a0bb8f2d88ffa0235866b5f6af
Wendy Wang <wendywangg928@gmail.com>
2023-12-13 01:12:36 -0500

    added loading spinner.jsx and message.jsx to show messages also added into homescreen and productscreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,8 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 function HomeScreen() {
   //replacing react useState and fetch request with redux apiSlice
@@ -18,9 +20,9 @@ function HomeScreen() {
   return (
     <>
       {isLoading ? (
-        <h2>Loading....</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
