dd66e628242f96991aa5ca871fbd4af304dc6f39
Wendy Wang <wendywangg928@gmail.com>
2023-12-14 20:39:58 -0500

    cleaned up code in cartSlice by creating a cartUtils.js and exporting from there

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,13 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 //JSON.parse ==> the process of converting a JSON object in text format to a Javascript object that can be used inside a program
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -23,25 +20,7 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
-      //Calculate total items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-      //Calculate shipping price ==> if order is over 100$ then free else its $10 shipping fee
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      //Calculate tax price ==> 15% tax
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-      //Calculate total price after shipping and tax
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
