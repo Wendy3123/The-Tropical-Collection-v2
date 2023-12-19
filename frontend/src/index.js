8c87c151d0af0682e0786b5be9ce340387e21982
Wendy Wang <wendywangg928@gmail.com>
2023-12-19 01:05:42 -0500

    loginScreen and FormContainer for sign in is complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ErrorScreen from "./screens/ErrorScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
       <Route path="*" element={<ErrorScreen />} />
     </Route>
   )
