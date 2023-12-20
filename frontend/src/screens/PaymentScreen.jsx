119d7ad53fe9cae7db04cd2bf86e098c041570ee
Wendy Wang <wendywangg928@gmail.com>
2023-12-20 03:15:04 -0500

    added backend routes and controller for orders

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,17 +1,37 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { savePaymentMethod } from "../slices/cartSlice";
 import { Form, Button, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
 
 function PaymentScreen() {
   const [paymentMethod, setPaymentMethod] = useState("Paypal");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
 
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as="legend">Select Method</Form.Label>
           <Col>
