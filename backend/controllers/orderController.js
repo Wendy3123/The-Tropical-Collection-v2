fd69e37e46b1650eac6aee3e6e17f9c68f2f7c45
Wendy Wang <wendywangg928@gmail.com>
2023-12-22 22:16:15 -0500

    added create new order route for orderController.js in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,38 @@ import asyncHandler from "../middleware/asyncHandler.js";
 //@route          POST /api/orders
 //@access         Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  //if there is an orderitems array but its empty
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 //@description    Get logged in user orders
