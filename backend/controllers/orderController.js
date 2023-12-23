0fc8e72e6a268bb8b0f348b81d54941028e83ef1
Wendy Wang <wendywangg928@gmail.com>
2023-12-22 22:41:32 -0500

    started placeOrder screen in frontend and finished get order by ID and get logged in user order backend route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,14 +43,26 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@route          GET /api/orders/myorders
 //@access         Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({
+    user: req.user._id,
+  });
+  res.status(200).json(orders);
 });
 
 //@description    Get order by ID
 //@route          GET /api/orders/:id
 //@access         Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by ID");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(400);
+    throw new Error("Order not found");
+  }
 });
 
 //@description    Update order to paid
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -36,6 +36,10 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       return updateCart(state);
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
+    },
   },
 });
 export const {
@@ -43,6 +47,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
