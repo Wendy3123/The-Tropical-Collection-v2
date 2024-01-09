d196cbcc5da2c855213ddf213802cda400c78106
Wendy Wang <wendywangg928@gmail.com>
2024-01-08 23:59:59 -0500

    changed backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,8 @@
-import Order from "../models/orderModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
+import Product from "../models/productModel.js";
+import { calcPrices } from "../utils/calcPrices.js";
+import { verifyPayPalPayment, checkIfNewTransaction } from "../utils/paypal.js";
 
 //@description    Creates new order
 //@route          POST /api/orders
